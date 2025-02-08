ef5b0317ce158838a2ccbcafed7c8059a169a142
kabo97 <alielmatarawy8@gmail.com>
2025-02-08 23:41:18 +0300

    React and node

--- cloud-app/client/src/App.js
+++ cloud-app/client/src/App.js
@@ -1,16 +1,29 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    const [message, setMessage] = useState("");
-
-    useEffect(() => {
-        axios.get("http://localhost:3001/")
-            .then((response) => setMessage(response.data.message))
-            .catch((error) => console.error("Error fetching data:", error));
-    }, []);
-
-    return <h1>{message}</h1>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGYyc3F0ZDBpMW9hZ3ltdGJrbWw4bnA0MzlvN3M2bmR6NjhpaXJvYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/p2GjsflFKS72fsUagg/giphy.gif"className="App-logo1" alt="logo" />
+        <h1 className="text-2xl font-bold mt-4">
+          Welcome to SWE-455 HW1
+        </h1>
+        
+        <p className="mt-2 text-lg">
+          This is my static web app for SWE-455 HW1 using React.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/ali-iessa-elmatarwy-0a734a240"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ali Elmatarwy
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
